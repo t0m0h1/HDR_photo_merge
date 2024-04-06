3f585b7e63771df00d5be6773450c248599be78d
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2024-04-06 22:26:55 +0100

    Refactor tonemap function to use createTonemapReinhard() and update image saving logic

--- app.py
+++ app.py
@@ -2,30 +2,36 @@ import cv2
 import numpy as np
 from PIL import Image
 
+
 def extract_exposure_times(image_paths):
     exposure_times = []
     for path in image_paths:
         with Image.open(path) as img:
             exif_data = img._getexif()
-            exposure_time = float(exif_data[33434].num) / float(exif_data[33437].den)  # Extracting exposure time from EXIF data
+            exposure_time = float(exif_data[33434])  # Extracting exposure time from EXIF data
             exposure_times.append(exposure_time)
     return np.array(exposure_times, dtype=np.float32)
+
 
 def merge_images(images, exposure_times):
     merge_debvec = cv2.createMergeDebevec()
     hdr = merge_debvec.process(images, times=exposure_times)
     return hdr
 
+
 def tonemap(hdr):
-    tonemap = cv2.createTonemapDurand(gamma=2.2)
+    tonemap = cv2.createTonemapReinhard()
     ldr = tonemap.process(hdr)
-    ldr = 3 * ldr
-    return np.clip(ldr, 0, 1)
+    ldr = np.clip(ldr, 0, 1)
+    return ldr
+
 
 if __name__ == "__main__":
+    print(cv2.__version__)
+
     # Paths to input images
     image_paths = ["images/DJI_0965.JPG", "images/DJI_0966.JPG", "images/DJI_0967.JPG"]
-    
+
     # Extract exposure times
     exposure_times = extract_exposure_times(image_paths)
 
@@ -39,4 +45,4 @@ if __name__ == "__main__":
     ldr = tonemap(hdr)
 
     # Save tonemapped image
-    cv2.imwrite("output_hdr.jpg", np.clip(ldr * 255, 0, 255).astype(np.uint8))
+    cv2.imwrite("output_hdr.jpg", (ldr * 255).astype(np.uint8))
